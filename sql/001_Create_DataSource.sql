b6f13727e5a31628ad601d47e5719126caafb5a1
mah-data <hatami54@gmail.com>
2026-07-27 10:06:19 +0330

    Update 001_Create_DataSource.sql

--- sql/001_Create_DataSource.sql
+++ sql/001_Create_DataSource.sql
@@ -1,3 +1,41 @@
+/*
+=========================================================
+Table : DataSource
+Version : 1.0
+Project : EDIP
+Author : Mahtab
+=========================================================
+
+Purpose
+-------
+Represents a logical business source of data.
+
+Description
+-----------
+A DataSource identifies the business owner of the data.
+It is independent of database technology.
+
+Architecture Decisions
+----------------------
+ADR-002
+DataSource is a Business Concept.
+
+ADR-003
+One DataSource can have many Connections.
+
+Design Decisions
+----------------
+✔ SourceType is stored as NVARCHAR.
+Reason:
+To simplify Version 1.0.
+Lookup table may be introduced in a future version.
+
+✔ Status is used instead of IsActive.
+Reason:
+Supports multiple operational states.
+
+=========================================================
+*/
 CREATE TABLE dbo.DataSource
 (
     DataSourceID INT IDENTITY(1,1) NOT NULL,
