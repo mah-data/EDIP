a996d604f13a07b1941e30fd2c7e12b414bd33ae
mah-data <hatami54@gmail.com>
2026-07-27 10:07:53 +0330

    Update 004_Create_DataQualityRule.sql

--- sql/004_Create_DataQualityRule.sql
+++ sql/004_Create_DataQualityRule.sql
@@ -1,1 +1,25 @@
+/*
+=========================================================
+Table : DataQualityRule
+=========================================================
 
+Purpose
+-------
+Stores validation rules.
+
+Architecture Decisions
+----------------------
+
+Each Rule belongs to one Dataset.
+
+Design Decisions
+----------------
+
+✔ Rules have Severity.
+
+✔ Rules may Block data.
+
+✔ RuleExpression stores executable rule.
+
+=========================================================
+*/
