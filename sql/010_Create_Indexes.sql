ec17bc3d2eafd4524538783fbd8377b133c114ea
mah-data <hatami54@gmail.com>
2026-07-28 08:13:26 +0330

    Update 010_Create_Indexes.sql

--- sql/010_Create_Indexes.sql
+++ sql/010_Create_Indexes.sql
@@ -21,3 +21,20 @@ CREATE NONCLUSTERED INDEX IX_Connection_ConnectionName
 ON dbo.Connection(ConnectionName);
 
 GO
+
+-- Dataset
+
+CREATE NONCLUSTERED INDEX IX_Dataset_ConnectionID
+ON dbo.Dataset(ConnectionID);
+
+GO
+
+CREATE NONCLUSTERED INDEX IX_Dataset_DatasetName
+ON dbo.Dataset(DatasetName);
+
+GO
+
+CREATE NONCLUSTERED INDEX IX_Dataset_ObjectName
+ON dbo.Dataset(ObjectName);
+
+GO
