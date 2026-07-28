bd6e9c0b889bbf4975b861001e21bd7b76073bdd
mah-data <hatami54@gmail.com>
2026-07-28 08:08:10 +0330

    Update 010_Create_Indexes.sql

--- sql/010_Create_Indexes.sql
+++ sql/010_Create_Indexes.sql
@@ -1,1 +1,23 @@
+/*========================================================
+EDIP
+Index Creation Script
+========================================================*/
 
+-- DataSource
+
+CREATE NONCLUSTERED INDEX IX_DataSource_SourceName
+ON dbo.DataSource(SourceName);
+
+GO
+
+-- Connection
+
+CREATE NONCLUSTERED INDEX IX_Connection_DataSourceID
+ON dbo.Connection(DataSourceID);
+
+GO
+
+CREATE NONCLUSTERED INDEX IX_Connection_ConnectionName
+ON dbo.Connection(ConnectionName);
+
+GO
