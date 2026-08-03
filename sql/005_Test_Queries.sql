ca52e2979df49899ee4e354b9e42205d89b0333e
mah-data <hatami54@gmail.com>
2026-08-04 00:31:16 +0330

    Complete EDIP Version 1 database implementation

--- sql/005_Test_Queries.sql
+++ sql/005_Test_Queries.sql
@@ -33,15 +33,24 @@ FROM dbo.DataQualityRule;
 
 GO
 -- Check Relations 
-  
+USE EDIP;
+GO
+
 SELECT
     ds.SourceName,
     c.ConnectionName,
-    d.DatasetName
+    d.DatasetName,
+    d.DatasetType,
+    r.RuleName,
+    r.Severity,
+    r.IsBlocking,
+    r.IsEnabled
 FROM dbo.DataSource ds
-
-INNER JOIN dbo.Connection c
-ON ds.DataSourceID = c.DataSourceID
-
+INNER JOIN dbo.[Connection] c
+    ON ds.DataSourceID = c.DataSourceID
 INNER JOIN dbo.Dataset d
-ON c.ConnectionID = d.ConnectionID;
+    ON c.ConnectionID = d.ConnectionID
+LEFT JOIN dbo.DataQualityRule r
+    ON d.DatasetID = r.DatasetID
+ORDER BY ds.SourceName, d.DatasetName;
+GO
